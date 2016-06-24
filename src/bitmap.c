025f459d052c910559dea69ea62bdb75a025515a
Douglas Thain <dthain@nd.edu>
2016-06-24 10:43:49 -0400

    Added missing bitmap module.

---
+++ src/bitmap.c
@@ -0,0 +1,45 @@
+/*
+Copyright (C) 2016 The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
+
+#include "bitmap.h"
+#include "kernelcore.h"
+#include "kmalloc.h"
+
+static struct bitmap root_bitmap;
+
+struct bitmap * bitmap_create_root()
+{
+	root_bitmap.width = video_xres;
+	root_bitmap.height = video_yres;
+	root_bitmap.format = BITMAP_FORMAT_RGB;
+	root_bitmap.data = video_buffer;
+	return &root_bitmap;
+}
+
+struct bitmap * bitmap_create( int width, int height, int format )
+{
+	struct bitmap *b = kmalloc(sizeof(*b));
+	if(!b) return 0;
+
+	b->data = kmalloc(width*height*3);
+	if(!b->data) {
+		kfree(b);
+		return 0;
+	}
+
+	b->width = width;
+	b->height = height;
+	b->format = format;
+
+	return b;
+}
+
+void bitmap_delete( struct bitmap *b )
+{
+	kfree(b->data);
+	kfree(b);
+}
+
---
+++ src/bitmap.h
@@ -0,0 +1,28 @@
+/*
+Copyright (C) 2016 The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
+
+#ifndef BITMAP_H
+#define BITMAP_H
+
+#include "kerneltypes.h"
+
+struct bitmap * bitmap_create_root();
+
+struct bitmap * bitmap_create( int width, int height, int format );
+void bitmap_delete( struct bitmap *b );
+
+struct bitmap {
+	uint32_t width;
+	uint32_t height;
+	uint32_t format;
+	uint8_t  *data;
+};
+
+#define BITMAP_FORMAT_RGB      0
+#define BITMAP_FORMAT_RGBA     1
+
+#endif
+
