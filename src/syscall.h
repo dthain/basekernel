827ad12bc55965c0bd5176454e0203a29600ddd5
Douglas Thain <dthain@nd.edu>
2016-06-24 10:44:00 -0400

    Added basic structure for unix-like system calls.

--- src/syscall.h
+++ src/syscall.h
@@ -9,19 +9,60 @@ See the file LICENSE for details.
 
 #include "kerneltypes.h"
 
-#define SYSCALL_exit     1
-#define SYSCALL_testcall 2
-#define SYSCALL_yield    3
+typedef enum {
+	SYSCALL_DEBUG,
+	SYSCALL_YIELD,
+	SYSCALL_FORK,
+	SYSCALL_EXEC,
+	SYSCALL_WAIT,
+	SYSCALL_EXIT,
+	SYSCALL_OPEN,
+	SYSCALL_READ,
+	SYSCALL_WRITE,
+	SYSCALL_LSEEK,
+	SYSCALL_CLOSE
+} syscall_t;
 
-uint32_t syscall( uint32_t n, uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t e );
+typedef enum {
+	ENOENT,
+	EINVAL,
+	EACCES,
+	ENOSYS
+} syscall_error_t;
 
-static inline int32_t exit( uint32_t status )
-	{ return syscall( SYSCALL_exit, status, 0, 0, 0, 0 ); }
+uint32_t syscall( syscall_t s, uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t e );
 
-static inline int32_t testcall( int x ) 
-	{ return syscall( SYSCALL_testcall, x, 0, 0, 0, 0 ); }
+static inline void debug( const char *str )
+{ syscall( SYSCALL_DEBUG, (uint32_t) str, 0, 0, 0, 0 ); }
 
-static inline int32_t yield()
-	{ return syscall( SYSCALL_yield, 0, 0, 0, 0, 0 ); }
+static inline void exit( int status )
+	{ syscall( SYSCALL_EXIT, status, 0, 0, 0, 0 ); }
+
+static inline int yield()
+	{ return syscall( SYSCALL_YIELD, 0, 0, 0, 0, 0 ); }
+
+static inline int fork()
+	{ return syscall( SYSCALL_FORK, 0, 0, 0, 0, 0 ); }
+
+static inline int exec( const char *cmd )
+	{ return syscall( SYSCALL_EXEC, (uint32_t) cmd, 0, 0, 0, 0 ); }
+
+static inline int wait()
+	{ return syscall( SYSCALL_WAIT, 0, 0, 0, 0, 0 ); }
+
+static inline int open( const char *path, int mode, int flags )
+	{ return syscall( SYSCALL_OPEN, (uint32_t) path, mode, flags, 0, 0 ); }
+
+static inline int read( int fd, void *data, int length )
+	{ return syscall( SYSCALL_READ, fd, (uint32_t) data, length, 0, 0 ); }
+
+static inline int write( int fd, void *data, int length )
+	{ return syscall( SYSCALL_WRITE, fd, (uint32_t) data, length, 0, 0 ); }
+
+static inline int lseek( int fd, int offset, int whence )
+	{ return syscall( SYSCALL_LSEEK, fd, offset, whence, 0, 0 ); }
+
+static inline int close( int fd )
+{ return syscall( SYSCALL_CLOSE, fd, 0, 0, 0, 0 ); }
 
 #endif
--- src/syscall_handler.c
+++ src/syscall_handler.c
@@ -8,30 +8,78 @@ See the file LICENSE for details.
 #include "console.h"
 #include "process.h"
 
-uint32_t sys_exit( uint32_t code )
+int sys_debug( const char *str )
 {
-	process_exit(code);
+	console_printf("%s",str);
 	return 0;
 }
 
-uint32_t sys_yield()
+int sys_exit( int status )
+{
+	process_exit(status);
+	return 0;
+}
+
+int sys_yield()
 {
 	process_yield();
 	return 0;
 }
 
-uint32_t sys_testcall( uint32_t code )
+int sys_fork()
 {
-	console_printf("testing: %d\n",code);
-	return 0;
+	return ENOSYS;
 }
 
-int32_t syscall_handler( uint32_t n, uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t e )
+int sys_exec( const char *cmd )
+{
+	return ENOSYS;
+}
+
+int sys_wait()
+{
+	return ENOSYS;
+}
+
+int sys_open( const char *path, int mode, int flags )
+{
+	return ENOSYS;
+}
+
+int sys_read( int fd, void *data, int length )
+{
+	return ENOSYS;
+}
+
+int sys_write( int fd, void *data, int length )
+{
+	return ENOSYS;
+}
+
+int sys_lseek( int fd, int offset, int whence )
+{
+	return ENOSYS;
+}
+
+int sys_close( int fd )
+{
+	return ENOSYS;
+}
+
+int32_t syscall_handler( syscall_t n, uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t e )
 {
 	switch(n) {
-	case SYSCALL_exit:	return sys_exit(a);
-	case SYSCALL_testcall:	return sys_testcall(a);
-	case SYSCALL_yield:	return sys_yield();
+	case SYSCALL_EXIT:	return sys_exit(a);
+	case SYSCALL_DEBUG:	return sys_debug((const char*)a);
+	case SYSCALL_YIELD:	return sys_yield();
+	case SYSCALL_FORK:	return sys_fork();
+	case SYSCALL_EXEC:	return sys_exec((const char *)a);
+	case SYSCALL_WAIT:	return sys_wait();
+	case SYSCALL_OPEN:	return sys_open((const char *)a,b,c);
+	case SYSCALL_READ:	return sys_read(a,(void*)b,c);
+	case SYSCALL_WRITE:	return sys_write(a,(void*)b,c);
+	case SYSCALL_LSEEK:	return sys_lseek(a,b,c);
+	case SYSCALL_CLOSE:	return sys_close(a);
 	default:		return -1;
 	}
 }
