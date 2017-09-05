0274f70b2269b0c510de4b997db67682790e7887
Kevin Wern <kevin.m.wern@gmail.com>
2017-09-05 01:11:49 -0400

    create file.c
    Really this is sort of a junk drawer of fs and file-like things. Right
    now, the primary purpose is to create in-memory inodes for and files
    for devices.

---
+++ src/file.c
@@ -0,0 +1,41 @@
+#include "file.h"
+#include "kmalloc.h"
+#include "string.h"
+#include "process.h"
+
+#define IN_MEMORY_CHUNK_SIZE 1024
+#define IN_MEMORY_INODE_COUNT 10
+
+static struct memory_inode_t *in_memory_fs;
+static char *in_memory_fs_data;
+static int memory_inode_count;
+
+int file_init()
+{
+	in_memory_fs = kmalloc(IN_MEMORY_INODE_COUNT * sizeof(struct memory_inode_t));
+	in_memory_fs_data = kmalloc(IN_MEMORY_CHUNK_SIZE * IN_MEMORY_INODE_COUNT);
+	memory_inode_count = 0;
+
+	return 0;
+}
+
+int register_memory_inode(const char *filename)
+{
+	struct memory_inode_t *new_node = in_memory_fs + memory_inode_count;
+	new_node->data_start = in_memory_fs_data +  memory_inode_count * IN_MEMORY_CHUNK_SIZE;
+	new_node->filename = filename;
+	memory_inode_count++;
+
+	return 0;
+}
+
+int file_memory_inode_index(const char *name)
+{
+	int i;
+	for (i = 0; i < memory_inode_count; i++)
+	{
+		if (!strcmp(name, in_memory_fs[i].filename))
+			return i;
+	}
+	return -1;
+}
