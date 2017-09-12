d477a83e6b9e80af28036662e921ec1125ed688d
Kevin Wern <kevin.m.wern@gmail.com>
2017-09-12 01:14:41 -0400

    file.h: add other operations to file struct

--- src/file.h
+++ src/file.h
@@ -25,8 +25,11 @@ struct file_t
 	uint32_t inode;
 	enum inode_type type;
 
+	int (*open)(struct file_t *, char *, uint8_t);
+	int (*close)(struct file_t *, int);
 	int (*read)(struct file_t *, char *, uint32_t);
 	int (*write)(struct file_t *, char *, uint32_t);
+	int (*seek)(struct file_t *, char *, uint32_t);
 };
 
 int file_init();
