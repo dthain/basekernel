485e8709855013865a3d0ba1faf40d8acde5a850
Kevin Wern <kevin.m.wern@gmail.com>
2017-10-08 18:09:25 -0400

    fs.h: create method stubs and abstract structs

---
+++ src/fs.h
@@ -0,0 +1,57 @@
+#ifndef FS_H
+#define FS_H
+
+#include "kerneltypes.h"
+
+struct fs {
+	char *name;
+	struct volume *(*mount)(uint32_t device_no);
+};
+
+struct file {
+	void *private_data;
+	uint32_t sz;
+	int (*read)(struct file *f, char *buffer, uint32_t n);
+	int (*write)(struct file *f, char *buffer, uint32_t n);
+	int (*close)(struct file *f);
+};
+
+struct volume {
+	void *private_data;
+	struct dirent *(*root)(struct volume *d);
+	int (*umount)(struct volume *d);
+};
+
+struct dirent {
+	void *private_data;
+	struct volume *v;
+	uint32_t sz;
+
+	struct file *(*open)(struct dirent *d, int8_t mode);
+	int (*close)(struct dirent *d);
+	struct dirent *(*mkdir)(struct dirent *d, const char *name);
+	struct dirent *(*lookup)(struct dirent *d, const char *name);
+	int (*readdir)(struct dirent *d, char *buffer, int buffer_length);
+	int (*rmdir)(struct dirent *d, const char *name);
+	int (*link)(struct dirent *d, const char *oldpath, const char *newpath);
+	int (*unlink)(struct dirent *d, const char *name);
+};
+
+struct fs *fs_get(const char *name);
+int fs_register(struct fs *f);
+
+struct volume *fs_mount(struct fs *f, uint32_t device_no);
+int fs_umount(struct volume *v);
+struct dirent *fs_root(struct volume *);
+
+struct file *fs_open(struct dirent *d, uint8_t mode);
+int fs_read(struct file *f, char *buffer, uint32_t n);
+int fs_close(struct dirent *d);
+struct dirent *fs_create(struct dirent *d, const char *name);
+struct dirent *fs_lookup(struct dirent *d, const char *name);
+int fs_readdir(struct dirent *d, char *buffer, int buffer_length);
+int fs_rmdir(struct dirent *d, const char *name);
+int fs_link(struct dirent *d, const char *oldpath, const char *newpath);
+int fs_unlink(struct dirent *d, const char *name);
+
+#endif
