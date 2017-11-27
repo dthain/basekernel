20a92c3a8f1835f25eaa1f2201acaa5f43d09086
Kevin Wern <kevin.m.wern@gmail.com>
2017-11-27 18:16:49 -0500

    write.exe: create
    simple program that opens a new file, writes to capacity, and then
    reads it.

---
+++ src/write.c
@@ -0,0 +1,50 @@
+/*
+Copyright (C) 2017 The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
+
+/*
+A trivial user level program to try out basic system calls.
+This program requires that write() and exit() work correctly.
+*/
+
+#include "syscalls.h"
+#include "string.h"
+#include "user-io.h"
+
+int main( const char *argv[], int argc )
+{
+	uint32_t j = 0;
+	int fd = open("kevin", 2, 0);
+	printf("got fd %d\n", fd);
+	printf("writing to file...\n");
+	for(;;) {
+		j++;
+		char buffer[100] = "Hello, world! I can write to ";
+		char num[5];
+		char newline[2] = "\n";
+		int n;
+		uint_to_string(j, num);
+		strcat(buffer,num);
+		strcat(buffer,newline);
+		n = write(fd, buffer, strlen(buffer));
+		if (n < 0) break;
+		printf("wrote %d chars: %s\n", n, buffer);
+	}
+	close(fd);
+	fd = open("kevin", 1, 0);
+	char buffer[100];
+	int n;
+	printf("reading file...\n");
+	while ((n = read(fd, buffer, 99)) > 0) {
+		buffer[n] = 0;
+		printf("%s", buffer);
+		flush();
+	}
+  
+	close(fd);
+	exit(0);
+  
+	return 0;
+}
