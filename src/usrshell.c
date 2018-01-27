7f1fca4b6c2e2e4ba2d46c2397a83233dd9c5e3e
8bit <tburchfi@nd.edu>
2018-01-27 18:46:41 -0500

    Begin input implementation

--- src/usrshell.c
+++ src/usrshell.c
@@ -1,8 +1,40 @@
 #include "string.h"
 #include "syscalls.h"
 #include "user-io.h"
+#include "ascii.h"
+
+int process_command(char * line) {
+  printf("processing command...\n");
+  return 0;
+}
 
 int main(char ** argv, int argc) {
-  printf("Hello user!\n");
+  printf("User shell ready:\n");
+  char line[1024];
+  char * pos = line;
+  char c;
+  printf("$ ");
+  while (1) {
+    c = keyboard_read_char();
+    if (pos == line && c == ASCII_BS)
+      continue;
+    printf_putchar(c);
+    draw_flush();
+    if (c == ASCII_CR) {
+      int res = process_command(line);
+      if (res < 0)
+        break;
+      pos = line;
+      printf("$ ");
+    }
+    else if (c == ASCII_BS) {
+      pos--;
+    }
+    else {
+      *pos = c;
+      pos++;
+    }
+    *pos = 0;
+  }
   return 0;
 }
