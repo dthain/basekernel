001bb10f170c679577d49c98a08d739375975e81
John Westhoff <jwesthof@nd.edu>
2018-02-19 02:06:14 -0500

    ELF reader (#87)
    * Started ELF reader
    
    * Added segment support to ELF readed
    
    * Finished basic ELF file loading
    
    * Fixed tab in makefile
    
    * Converted ELF loader from reading sections to reading programs
    
    * Added ELF module
    
    * Split ELF functions
    
    * Re-tabbed elf.c
    
    * Added process_inherit

--- src/syscall_handler.c
+++ src/syscall_handler.c
@@ -9,6 +9,7 @@ See the file LICENSE for details.
 #include "console.h"
 #include "keyboard.h"
 #include "process.h"
+#include "kmalloc.h"
 #include "cdromfs.h"
 #include "memorylayout.h"
 #include "graphics_lib.h"
@@ -16,6 +17,7 @@ See the file LICENSE for details.
 #include "fs.h"
 #include "clock.h"
 #include "rtc.h"
+#include "elf.h"
 #include "kmalloc.h"
 
 int sys_debug( const char *str )
@@ -46,56 +48,15 @@ Takes in argv and argc for the new process' main
 
 int sys_process_run( const char *path, const char** argv, int argc )
 {
-	/* Open and find the named path, if it exists. */
-
-	if(!root_directory) return ENOENT;
-
-	struct fs_dirent *d = fs_dirent_namei(root_directory,path);
-	if(!d) {
-		return ENOENT;
-	}
-
-	int length = d->sz;
-
-	/* Create a new process with enough pages for the executable and one page for the stack */
-
-	struct process *p = process_create(length,PAGE_SIZE*2);
-	if(!p) return ENOENT;
-
-	/* Round up length of the executable to an even pages */
-
-	int i;
-	int npages = length/PAGE_SIZE + (length%PAGE_SIZE ? 1 : 0);
-
-	struct fs_file *f = fs_file_open(d, FS_FILE_READ);
-
-	/* For each page, load one page from the file.  */
-	/* Notice that the cdrom block size (2048) is half the page size (4096) */
-
-	for(i=0;i<npages;i++) {
-		unsigned vaddr = PROCESS_ENTRY_POINT + i * PAGE_SIZE;
-		unsigned paddr;
-
-		pagetable_getmap(p->pagetable,vaddr,&paddr);
-		fs_file_read(f,(void*)paddr, PAGE_SIZE);
-	}
-
-	/* Close everything up */
-	
-	fs_dirent_close(d);
-	fs_file_close(f);
-
-    /* Copy open windows */
-    memcpy(p->windows, current->windows, sizeof(p->windows));
-    p->window_count = current->window_count;
-    for(i=0;i<p->window_count;i++) {
-        p->windows[i]->count++;
+	struct process *p = elf_load(path);
+    
+    if (!p) {
+        return ENOENT;
     }
+
+    process_inherit(p);
     process_pass_arguments(p, argv, argc);
 
-  
-    /* Set the parent of the new process to the calling process */
-    p->ppid = process_getpid();
 
 	/* Put the new process into the ready list */
 
