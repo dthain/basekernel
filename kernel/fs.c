d1024acac2cdc75d7f55ded2918618656a173c2d
Douglas Thain <dthain@nd.edu>
2018-05-30 16:04:25 -0400

    Check method existence on each filesystem

--- kernel/fs.c
+++ kernel/fs.c
@@ -29,13 +29,15 @@ struct fs *fs_lookup(const char *name)
 
 int fs_mkfs(struct fs *f, uint32_t device_no)
 {
-	if(!f) return EINVAL;
+	const struct fs_ops *ops = f->ops;
+	if(!ops->mkfs) return EOPNOTSUPP;
 	return f->ops->mkfs(device_no);
 }
 
 struct fs_volume *fs_volume_open(struct fs *f, uint32_t device_no)
 {
-	if(!f) return 0;
+	const struct fs_ops *ops = f->ops;
+	if(ops->mount) return 0;
 	return f->ops->mount(device_no);
 }
 
@@ -46,15 +48,18 @@ void fs_volume_addref(struct fs_volume *v)
 
 int fs_volume_close(struct fs_volume *v)
 {
-	if(!v) return EINVAL;
+	const struct fs_ops *ops = v->fs->ops;
+	if(!ops->umount) return EOPNOTSUPP;
+
 	v->refcount--;
-	if(v->refcount<0) return v->fs->ops->umount(v);
+	if(v->refcount<=0) return v->fs->ops->umount(v);
 	return -1;
 }
 
 struct fs_dirent *fs_volume_root(struct fs_volume *v)
 {
-	if(!v) return 0;
+	const struct fs_ops *ops = v->fs->ops;
+	if(!ops->root) return 0;
 
 	struct fs_dirent *res = v->fs->ops->root(v);
 	res->v = v;
@@ -63,22 +68,26 @@ struct fs_dirent *fs_volume_root(struct fs_volume *v)
 
 int fs_dirent_readdir(struct fs_dirent *d, char *buffer, int buffer_length)
 {
-	if(!d) return EINVAL;
-	return d->v->fs->ops->readdir(d, buffer, buffer_length);
+	const struct fs_ops *ops = d->v->fs->ops;
+	if(!ops->readdir) return EOPNOTSUPP;
+	return ops->readdir(d, buffer, buffer_length);
 }
 
 static struct fs_dirent *fs_dirent_lookup(struct fs_dirent *d, const char *name)
 {
-	if(!d || !name) return 0;
+	const struct fs_ops *ops = d->v->fs->ops;
+	if(!ops->lookup) return 0;
 
-	struct fs_dirent *res = d->v->fs->ops->lookup(d, name);
+	struct fs_dirent *res = ops->lookup(d, name);
 	res->v = d->v;
 	return res;
 }
 
 int fs_dirent_compare(struct fs_dirent *d1, struct fs_dirent *d2, int *result)
 {
-	if(!d1 || !d2) return EINVAL;
+	const struct fs_ops *ops = d1->v->fs->ops;
+	if(!ops->compare) return EOPNOTSUPP;
+
 	return d1->v->fs->ops->compare(d1, d2, result);
 }
 
@@ -108,10 +117,12 @@ void fs_dirent_addref(struct fs_dirent *d)
 
 int fs_dirent_close(struct fs_dirent *d)
 {
-	if(!d) return EINVAL;
+	const struct fs_ops *ops = d->v->fs->ops;
+	if(!ops->close) return EOPNOTSUPP;
+
 	d->refcount--;
-	if(d->refcount<0) {
-		return d->v->fs->ops->close(d);
+	if(d->refcount<=0) {
+		return ops->close(d);
 	} else {
 		return 0;
 	}
@@ -119,7 +130,8 @@ int fs_dirent_close(struct fs_dirent *d)
 
 struct fs_file *fs_file_open(struct fs_dirent *d, uint8_t mode)
 {
-	if(!d) return 0;
+	const struct fs_ops *ops = d->v->fs->ops;
+	if(!ops->open) return 0;
 
 	struct fs_file *f = kmalloc(sizeof(*f));
 	f->size = d->size;
@@ -146,15 +158,13 @@ int fs_file_close(struct fs_file *f)
 	return 0;
 }
 
-static int fs_file_read_block(struct fs_file *f, char *buffer, uint32_t blocknum)
-{
-	return f->d->v->fs->ops->read_block(f->d, buffer, blocknum);
-}
-
 int fs_file_read(struct fs_file *file, char *buffer, uint32_t length, uint32_t offset)
 {
 	int total = 0;
 	int bs = file->d->v->block_size;
+
+	const struct fs_ops *ops = file->d->v->fs->ops;
+	if(!ops->read_block) return EINVAL;
 
 	if(offset > file->size) {
 		return 0;
@@ -173,17 +183,17 @@ int fs_file_read(struct fs_file *file, char *buffer, uint32_t length, uint32_t o
 		int actual = 0;
 
 		if(offset % bs) {
-			actual = fs_file_read_block(file, temp, blocknum);
+			actual = ops->read_block(file->d, temp, blocknum);
 			if(actual != bs)
 				goto failure;
 			actual = MIN(bs - offset % bs,length);
 			memcpy(buffer, &temp[offset % bs], actual);
 		} else if(length >= bs) {
-			actual = fs_file_read_block(file, buffer, blocknum);
+			actual = ops->read_block(file->d, buffer, blocknum);
 			if(actual != bs)
 				goto failure;
 		} else {
-			actual = fs_file_read_block(file, temp, blocknum);
+			actual = ops->read_block(file->d, temp, blocknum);
 			if(actual != bs)
 				goto failure;
 			actual = length;
@@ -208,37 +218,39 @@ int fs_file_read(struct fs_file *file, char *buffer, uint32_t length, uint32_t o
 
 int fs_dirent_mkdir(struct fs_dirent *d, const char *name)
 {
-	if(!d || !name) return EINVAL;
-	return d->v->fs->ops->mkdir(d, name);
+	const struct fs_ops *ops = d->v->fs->ops;
+	if(!ops->mkdir) return 0;
+	return ops->mkdir(d, name);
 }
 
 int fs_dirent_mkfile(struct fs_dirent *d, const char *name)
 {
-	if(!d || !name) return EINVAL;
-	return d->v->fs->ops->mkfile(d, name);
+	const struct fs_ops *ops = d->v->fs->ops;
+	if(!ops->mkfile) return 0;
+	return ops->mkfile(d, name);
 }
 
 int fs_dirent_rmdir(struct fs_dirent *d, const char *name)
 {
-	if(!d || !name) return EINVAL;
-	return d->v->fs->ops->rmdir(d, name);
+	const struct fs_ops *ops = d->v->fs->ops;
+	if(!ops->rmdir) return 0;
+	return ops->rmdir(d, name);
 }
 
 int fs_dirent_unlink(struct fs_dirent *d, const char *name)
 {
-	if(!d || !name) return EINVAL;
-	return d->v->fs->ops->unlink(d, name);
-}
-
-static int fs_file_write_block(struct fs_file *f, const char *buffer, uint32_t blocknum)
-{
-	return f->d->v->fs->ops->write_block(f->d, buffer, blocknum);
+	const struct fs_ops *ops = d->v->fs->ops;
+	if(!ops->unlink) return 0;
+	return ops->unlink(d, name);
 }
 
 int fs_file_write(struct fs_file *file, const char *buffer, uint32_t length, uint32_t offset)
 {
 	int total = 0;
 	int bs = file->d->v->block_size;
+
+	const struct fs_ops *ops = file->d->v->fs->ops;
+	if(!ops->write_block || !ops->read_block) return EINVAL;
 
 	char *temp = memory_alloc_page(0);
 
@@ -248,30 +260,30 @@ int fs_file_write(struct fs_file *file, const char *buffer, uint32_t length, uin
 		int actual = 0;
 
 		if(offset % bs) {
-			actual = fs_file_read_block(file, temp, blocknum);
+			actual = ops->read_block(file->d, temp, blocknum);
 			if(actual != bs)
 				goto failure;
 
 			actual = MIN(bs - offset % bs,length);
 			memcpy(&temp[offset % bs], buffer, actual);
 
-			int wactual = fs_file_write_block(file, temp, blocknum);
+			int wactual = ops->write_block(file->d, temp, blocknum);
 			if(wactual != bs)
 				goto failure;
 
 		} else if(length >= bs) {
-			actual = fs_file_write_block(file, buffer, blocknum);
+			actual = ops->write_block(file->d, buffer, blocknum);
 			if(actual != bs)
 				goto failure;
 		} else {
-			actual = fs_file_read_block(file, temp, blocknum);
+			actual = ops->read_block(file->d, temp, blocknum);
 			if(actual != bs)
 				goto failure;
 
 			actual = length;
 			memcpy(temp, buffer, actual);
 
-			int wactual = fs_file_write_block(file, temp, blocknum);
+			int wactual = ops->write_block(file->d, temp, blocknum);
 			if(wactual != bs)
 				goto failure;
 		}
