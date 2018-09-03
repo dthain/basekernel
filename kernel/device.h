a1bb78729ad4d988f7bcc18ab955ea9c2beead35
Douglas Thain <dthain@nd.edu>
2018-09-03 15:49:52 -0400

    Removed unused device subset functionality. (Subsetting will happen at the higher level of kernel objects.)

--- kernel/device.h
+++ kernel/device.h
@@ -15,22 +15,14 @@
 struct device {
     int (*read) (struct device *d, void *buffer, int size, int offset);
     int (*write) (struct device *d, const void *buffer, int size, int offset);
-    struct device *(*subset) (struct device* d, int dx0, int dy0, int dx1, int dy1);
-
     int unit;
     int block_size;
-    int sx0;
-    int sy0;
-    int sx1;
-    int sy1;
     int alloced;
     struct buffer *buffer;
 };
 
 void device_init();
 struct device *device_open(char *type, int unit);
-struct device *device_subset(struct device *d, int dx0, int dy0, int dx1, int dy1);
-
 int device_read(struct device *d, void *buffer, int size, int offset);
 int device_write(struct device *d, const void *buffer, int size, int offset);
 
