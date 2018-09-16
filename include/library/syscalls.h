554639d01802bb1a703542536fd1d96eebbdb3b3
Jack Mazanec <jmazane1@nd.edu>
2018-09-16 18:16:24 -0400

    added non blocking argument for keyboard_read_char

--- include/library/syscalls.h
+++ include/library/syscalls.h
@@ -29,7 +29,7 @@ int write( int fd, void *data, int length );
 int lseek( int fd, int offset, int whence );
 int close( int fd );
 extern void* sbrk( int a );
-char keyboard_read_char();
+char keyboard_read_char(int non_blocking); // 1 for non-blocking, 0 for blocking
 int open_window( int wd, int x, int y, int w, int h );
 int set_blocking( int fd, int b );
 int console_open( int fd );
