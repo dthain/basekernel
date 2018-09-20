396f57c5c3c11a266d4ab5d9f55a46b7a777774b
Jack Mazanec <jmazane1@nd.edu>
2018-09-19 21:26:38 -0400

    initial commit

---
+++ user/alloctst.c
@@ -0,0 +1,36 @@
+/*
+Copyright (C) 2017 The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
+
+/*
+A fun graphics demo that features text bouncing around the screen.
+*/
+
+#include "library/syscalls.h"
+#include "library/user-io.h"
+
+int main() {
+	// Test 1: Check if we can allocate a large 2D array on the stack
+	int t_size = 500;
+	int test_1_array [t_size][t_size];
+	for (int i = 0; i < t_size; ++i) {
+		for (int j = 0; j < t_size; ++j) {
+			test_1_array[i][j] = 19; 
+		}
+	}
+	printf_putstring("Test 1 passed!\n");
+	flush();
+
+	// Test 2: Check that allocating large amounts on the heap and stack
+	// simultaneously does not cause any errors
+	char * test_2_string = malloc(5000);
+	int test_2_array[100000];
+	test_2_string = "test 2";
+	test_2_array[17632] = 4;
+	printf_putstring("Test 2 passed!\n");
+	flush();
+
+	return 0;
+}
