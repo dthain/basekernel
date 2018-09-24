ba356362bcc96a7c263351003104cde67e5b202b
doug <dsmith47@nd.edu>
2018-09-24 04:25:58 -0400

    moved file description test to a better-named location

--- user/filedescribetest.c
+++ user/filedescribetest.c
@@ -12,7 +12,7 @@ enum {
 
 int main(const char ** argv, int argc) {
   printf("Generating/typing all file descriptors.\n");
-  int type = 0;
+  int type = -2;
   int window_descriptor = open_window(KNO_STDWIN, 1, 1, 1, 1);
   if (!window_descriptor) {
     return 1;
@@ -20,11 +20,14 @@ int main(const char ** argv, int argc) {
   type = file_describe(window_descriptor);
   printf("Window file %d is of type: %d\n", window_descriptor, type);
 
+  /*
+   * Presently, the following code returns the same error as the file open
+   * demo.
+   * Can't be used until that bug is addressed.
   int reg_file_descriptor = open("testfile.txt", 1, 0);
   type = file_describe(reg_file_descriptor);
   printf("Text file %d is of type: %d\n", reg_file_descriptor, type);
-
-  
+  */
 
   return 0;
 }
