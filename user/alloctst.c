792c8a1dcf080ef5d6e35c33ae68dd7e835537f3
Jack Mazanec <jmazane1@nd.edu>
2018-10-03 14:37:39 -0400

    adding test cases

--- user/alloctst.c
+++ user/alloctst.c
@@ -15,49 +15,51 @@ Test to check that we are able to allocate more than 1 page of memory on the sta
 
 int func1();
 
-int main() {
-	// Test 1: Check if we can allocate a large 2D array on the stack
-	int t_size = 500, i, j;
-	int test_1_array_1 [t_size][t_size];
-	int test_1_array_2 [t_size][t_size];
-	for (i = 0; i < t_size; ++i) {
-		for (j = 0; j < t_size; ++j) {
-			test_1_array_1[i][j] = i%10; 
-			test_1_array_2[i][j] = i%15; 
-		}
-	}
-	printf_putstring("Test 1 passed!\n");
-	flush();
+// int main() {
+// 	// // Test 1: Check if we can allocate a large 2D array on the stack
+	// int t_size = 500, i, j;
+	// int test_1_array_1 [t_size][t_size];
+	// int test_1_array_2 [t_size][t_size];
+	// for (i = 0; i < t_size; ++i) {
+	// 	for (j = 0; j < t_size; ++j) {
+	// 		test_1_array_1[i][j] = i%10; 
+	// 		test_1_array_2[i][j] = i%15; 
+	// 	}
+	// }
+	// printf_putstring("Test 1 passed!\n");
+	// flush();
 
 
-	// Test 2: Check that allocating on stack does not interfere
-	int test_2_array_1[10000];
-	int * test_2_array_2 = malloc(10000);
+	// // Test 2: Check that allocating on stack does not interfere
+	// int test_2_array_1[10000];
+	// int * test_2_array_2 = malloc(10000);
 
-	for (i = 0; i < 10000; ++i)
-	{
-		test_2_array_1[i] = i%10;
-		test_2_array_2[i] = i%15;
-	}
+	// for (i = 0; i < 10000; ++i)
+	// {
+	// 	test_2_array_1[i] = i%10;
+	// 	test_2_array_2[i] = i%15;
+	// }
 
-	free(test_2_array_2);
+	// free(test_2_array_2);
 
-	printf_putstring("Test 2 passed!\n");
-	flush();	
+	// printf_putstring("Test 2 passed!\n");
+	// flush();	
 
 
-	// Test 3: Make sure calls to other functions works
-	func1();
-	int test_3_array[100000];
-	for (i = 0; i < 100000; ++i) {
-		test_3_array[i] = i%10;
-	}
+	// // Test 3: Make sure calls to other functions works
+	// func1();
+	// int test_3_array[100000];
+	// for (i = 0; i < 100000; ++i) {
+	// 	test_3_array[i] = i%10;
+	// }
 	
-	printf_putstring("Test 3 passed!\n");
-	flush();	
+	// printf_putstring("Test 3 passed!\n");
+	// flush();	
 
 	// Test 4: Test for failure of malloc
-	
+int main() {
+	func1();
+	// printf("Stack address %x\n");
 	unsigned sp;
 	asm( "mov %%esp, %0" : "=rm" ( sp ));
 	int * temp;
