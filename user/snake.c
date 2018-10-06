5d76f17b1c2ba4a75b158fbf72e43ffe171edceb
Jack Mazanec <jmazane1@nd.edu>
2018-10-06 17:13:57 -0400

    added widget library

--- user/snake.c
+++ user/snake.c
@@ -6,9 +6,9 @@ See the file LICENSE for details.
 
 /* Snake Game */
 #include "library/string.h"
-
 #include "library/syscalls.h"
 #include "library/user-io.h"
+#include "library/widgets.h"
 
 typedef unsigned int uint32_t;
 
@@ -201,25 +201,6 @@ int initialize_window(uint16_t x_b, uint16_t y_b, uint16_t w_b, uint16_t h_b, ui
 	draw_flush();
 
 	return wd;
-}
-
-// Draws the border with the colors specified
-int draw_border(int x, int y, int w, int h, int thickness, int screen_d_w, int screen_d_h, int r, int g, int b)
-{
-	// Check that dimensions line up 
-	if(x + w > screen_d_w || y + h > screen_d_h) {
-		return -1;
-	}
-	// Color the border appropriately
-	draw_color(r, b, g);
-
-	// Draw 4 rectangles to represent the border
-	draw_rect(0, 0, w, thickness);
-	draw_rect(0, 0, thickness, h);
-	draw_rect(w - thickness, 0, thickness, h);
-	draw_rect(0, h - thickness, w, 5);
-
-	return 0;
 }
 
 void draw_board(uint16_t wd, uint16_t x_0, uint16_t y_0, uint16_t game_width, uint16_t game_height, uint16_t x_steps, uint16_t y_steps, struct coords *snake_coords, struct coords apple, uint16_t thick)
