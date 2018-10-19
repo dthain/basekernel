bd77dd2e7561c9c6ff2e7edcd5e430231e637862
Jack Mazanec <jmazane1@nd.edu>
2018-10-18 21:51:52 -0400

    check syscall

--- user/awind.c
+++ user/awind.c
@@ -4,40 +4,26 @@
 
 
 int main() {
+	/* Test get_dimensions syscall */
+	int t[4];
+	int wd = 3; // open_window(3, 500, 500, 524, 200);
+	// int t2 = get_dimensions(wd, t);
+	draw_window(wd);
+	draw_string(600, 600, "Press any key to start");
+	draw_flush();
 
 
-	uint32_t t[4];
-	int t2 = get_window_properties(3, t);
+	process_sleep(10000);
 
+	// if (t2 < 0) {
+	// 	printf("Didnt work\n");
+	// 	return 1;
+	// }
 
-	if (t2 < 0) {
-		printf("Didnt work\n");
-		return 1;
-	}
-
-	printf("X: %d\n", t[0]);
-	printf("Y: %d\n", t[1]);
-	printf("W: %d\n", t[2]);
-	printf("H: %d\n", t[3]);
-
-
-	int wd = open_window(3, 500, 500, 524, 200);
-	t2 = get_window_properties(wd, t);
-
-	if (t2 < 0) {
-		printf("Didnt work\n");
-		return 1;
-	}
-
-	printf("X: %d\n", t[0]);
-	printf("Y: %d\n", t[1]);
-	printf("W: %d\n", t[2]);
-	printf("H: %d\n", t[3]);
-
-	// printf("%d\n", wd);
-	// process_fork();
-	// const char *args[] = { "snake.exe" };
-	// process_exec("snake.exe", args, 1);
+	// printf("X: %d\n", t[0]);
+	// printf("Y: %d\n", t[1]);
+	// printf("W: %d\n", t[2]);
+	// printf("H: %d\n", t[3]);
 
 	return 0;
 }
