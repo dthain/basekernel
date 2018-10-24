23a7526ad0317cdfe1e2b785ef7d79b86152b32f
Jack Mazanec <jmazane1@nd.edu>
2018-10-23 22:31:05 -0400

    added test for failure

--- user/awind.c
+++ user/awind.c
@@ -8,28 +8,30 @@ int main() {
 	int t1[4];
 	int wd = 3;
 
-	if (get_dimensions(wd, t1) < 0) {
+	if (get_dimensions(wd, t1, 2) < 0) {
 		printf("Didnt work\n");
 		return 1;
 	}
 
-	printf("X: %d\n", t1[0]);
-	printf("Y: %d\n", t1[1]);
-	printf("W: %d\n", t1[2]);
-	printf("H: %d\n", t1[3]);
+	printf("W: %d\n", t1[0]);
+	printf("H: %d\n", t1[1]);
 
 	/* Test get dimensions for a file */
-	int t2[2];
+	int t2[1];
 	const char * path = "./bin/snake.exe";
 	int fd = open(path, 0, 0);
 
-	if (get_dimensions(fd, t2) < 0) {
+	if (get_dimensions(fd, t2, 1) < 0) {
 		printf("Didnt work\n");
 		return 1;
 	}
 
 	printf("Size:     %d\n", t2[0]);
-	printf("Refcount: %d\n", t2[1]);
+
+	/* Test get_dimensions failure */
+	if (get_dimensions(fd, t2, 2) == 0) {
+		printf("Fail worked\n");
+	}
 
 	return 0;
 }
