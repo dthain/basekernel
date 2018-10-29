af8823fe8452ace4019aa913d87d5fa15c093e23
Doug Smith <dougsmith2015@gmail.com>
2018-10-29 13:14:10 -0400

    filedescribetest should now display type strings.

--- user/filedescribetest.c
+++ user/filedescribetest.c
@@ -2,24 +2,23 @@
 #include "library/string.h"
 #include "library/user-io.h"
 
-enum {
-	KOBJECT_INVALID = 0,
-	KOBJECT_FILE,
-	KOBJECT_DEVICE,
-	KOBJECT_GRPAHICS,
-	KOBJECT_PIPE
-};
-
 int main(const char **argv, int argc)
 {
 	printf("Generating/typing all file descriptors.\n");
 	int type = -2;
+	char * type_string = "A type string";
+	printf("     This translates to type: %s\n", type_string);
+	printf("     This translates to type: %s\n", type_string);
+	printf("     This translates to type: %s\n", type_string);
+	printf("     This translates to type: %s\n", type_string);
 	int window_descriptor = open_window(KNO_STDWIN, 1, 1, 1, 1);
 	if(!window_descriptor) {
 		return 1;
 	}
 	type = object_type(window_descriptor);
 	printf("Window file %d is of type: %d\n", window_descriptor, type);
+	type_string = strobjtype(type);
+	printf("     This translates to type: %s\n", type_string);
 
 	/*
 	 * Presently, the following code returns the same error as the file open
