957f07c165cdfb5e7f524502a2cf3277e73f1942
Doug Smith <dougsmith2015@gmail.com>
2018-11-06 22:08:26 -0500

    Error code => string conversion function

--- include/library/errno.h
+++ include/library/errno.h
@@ -1,6 +1,20 @@
 #ifndef ERRNO_H
 #define ERRNO_H
 
-void strerror(int err_code, char * output);
+static const char KERROR_NOT_FOUND_STRING[] = "KERROR: Not Found";
+static const char KERROR_INVALID_REQUEST_STRING[] = "KERROR: Invalid Request";
+static const char KERROR_PERMISSION_DENIED_STRING[] = "KERROR: Permission Denied";
+static const char KERROR_NOT_IMPLEMENTED_STRING[] = "KERROR: Not Implemented";
+static const char KERROR_NOT_EXECUTABLE_STRING[] = "KERROR: Not Executable";
+static const char KERROR_EXECUTION_FAILED_STRING[] = "KERROR: Execution Failed";
+static const char KERROR_NOT_SUPPORTED_STRING[] = "KERROR: Not a Supported String";
+static const char KERROR_NOT_A_DIRECTORY_STRING[] = "KERROR: Not a Directory";
+static const char KERROR_NOT_A_FILE_STRING[] = "KERROR: Not a File";
+static const char KERROR_NOT_A_WINDOW_STRING[] = "KERROR: Not a Window";
+static const char KERROR_NOT_A_DEVICE_STRING[] = "KERROR: Not a Device";
+static const char KERROR_NO_MEMORY_STRING[] = "KERROR: No Memory";
+static const char KERROR_IO_FAILURE_STRING[] = "KERROR: IO Failure";
+
+const char * strerror(int err_code);
 
 #endif
--- library/errno.c
+++ library/errno.c
@@ -2,58 +2,44 @@
 #include "library/errno.h"
 #include "library/string.h"
 
-const char * KERROR_NOT_FOUND_STRING = "KERROR: Not Found";
-const char * KERROR_INVALID_REQUEST_STRING = "KERROR: Invalid Request";
-const char * KERROR_PERMISSION_DENIED_STRING = "KERROR: Permission Denied";
-const char * KERROR_NOT_IMPLEMENTED_STRING = "KERROR: Not Implemented";
-const char * KERROR_NOT_EXECUTABLE_STRING = "KERROR: Not Executable";
-const char * KERROR_EXECUTION_FAILED_STRING = "KERROR: Execution Failed";
-const char * KERROR_NOT_SUPPORTED_STRING = "KERROR: Not a Supported String";
-const char * KERROR_NOT_A_DIRECTORY_STRING = "KERROR: Not a Directory";
-const char * KERROR_NOT_A_FILE_STRING = "KERROR: Not a File";
-const char * KERROR_NOT_A_WINDOW_STRING = "KERROR: Not a Window";
-const char * KERROR_NOT_A_DEVICE_STRING = "KERROR: Not a Device";
-const char * KERROR_NO_MEMORY_STRING = "KERROR: No Memory";
-const char * KERROR_IO_FAILURE_STRING = "KERROR: IO Failure";
-
-void strerror(int err_code, char * output) {
+const char * strerror(int err_code) {
 	if (err_code == KERROR_NOT_FOUND) {
-  		strcpy(output, KERROR_NOT_FOUND_STRING);
+  		return KERROR_NOT_FOUND_STRING;
 	}
 	if (err_code == KERROR_INVALID_REQUEST) {
-  		strcpy(output, KERROR_INVALID_REQUEST_STRING);
+  		return KERROR_INVALID_REQUEST_STRING;
 	}
 	if (err_code == KERROR_PERMISSION_DENIED) {
-  		strcpy(output, KERROR_PERMISSION_DENIED_STRING);
+  		return KERROR_PERMISSION_DENIED_STRING;
 	}
 	if (err_code == KERROR_NOT_IMPLEMENTED) {
-  		strcpy(output, KERROR_NOT_IMPLEMENTED_STRING);
+  		return KERROR_NOT_IMPLEMENTED_STRING;
 	}
 	if (err_code == KERROR_NOT_EXECUTABLE) {
-  		strcpy(output, KERROR_NOT_EXECUTABLE_STRING);
+  		return KERROR_NOT_EXECUTABLE_STRING;
 	}
 	if (err_code == KERROR_EXECUTION_FAILED) {
-  		strcpy(output, KERROR_EXECUTION_FAILED_STRING);
+  		return KERROR_EXECUTION_FAILED_STRING;
 	}
 	if (err_code == KERROR_NOT_SUPPORTED) {
-  		strcpy(output, KERROR_NOT_SUPPORTED_STRING);
+  		return KERROR_NOT_SUPPORTED_STRING;
 	}
 	if (err_code == KERROR_NOT_A_DIRECTORY) {
-  		strcpy(output, KERROR_NOT_A_DIRECTORY_STRING);
+  		return KERROR_NOT_A_DIRECTORY_STRING;
 	}
 	if (err_code == KERROR_NOT_A_FILE) {
-  		strcpy(output, KERROR_NOT_A_FILE_STRING);
+  		return KERROR_NOT_A_FILE_STRING;
 	}
 	if (err_code == KERROR_NOT_A_WINDOW) {
-  		strcpy(output, KERROR_NOT_A_WINDOW_STRING);
+  		return KERROR_NOT_A_WINDOW_STRING;
 	}
 	if (err_code == KERROR_NOT_A_DEVICE) {
-  		strcpy(output, KERROR_NOT_A_DEVICE_STRING);
+  		return KERROR_NOT_A_DEVICE_STRING;
 	}
 	if (err_code == KERROR_NO_MEMORY) {
-  		strcpy(output, KERROR_NO_MEMORY_STRING);
+  		return KERROR_NO_MEMORY_STRING;
 	}
 	if (err_code == KERROR_IO_FAILURE) {
-  		strcpy(output, KERROR_IO_FAILURE_STRING);
+  		return KERROR_IO_FAILURE_STRING;
 	}
 }
