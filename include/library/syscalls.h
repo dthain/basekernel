9c9b978be88b41a11886ff4b11160e220267047a
Jack Mazanec <jmazane1@nd.edu>
2018-11-22 22:45:45 -0500

    modified wrun to take fd

--- include/library/syscalls.h
+++ include/library/syscalls.h
@@ -46,6 +46,6 @@ int sys_stats(struct sys_stats *s);
 int process_stats(struct proc_stats *s, unsigned int pid);
 uint32_t gettimeofday();
 int get_dimensions(int fd, int * dims, int n);
-int process_wrun(const char *cmd, const char **argv, int argc, int window_description[4], int wd);
+int process_wrun(const char *cmd, const char **argv, int argc, int wd);
 
 #endif
