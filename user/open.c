87571ed22117535ba6bdba4ab9bd87fc4b89f8f2
Doug Smith <dougsmith2015@gmail.com>
2018-11-30 15:19:20 -0500

    Updated open.c and user/Makefile with open() syscall tests

---
+++ user/open.c
@@ -0,0 +1,33 @@
+/*
+Copyright (C) 2017 The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
+
+/*
+A trivial user level program to try out basic system calls.
+*/
+
+#include "library/syscalls.h"
+#include "library/string.h"
+#include "library/user-io.h"
+
+int main(const char *argv[], int argc)
+{
+	uint32_t j = 0;
+	chdir("/");
+	printf("got root\n");
+	int fd = open("kevin", 1, 0);
+	char buffer[1000];
+	int n;
+	printf("reading file...\n");
+	while((n = read(fd, buffer, 999)) > 0) {
+		buffer[n] = 0;
+		printf("%s");
+		flush();
+	}
+	close(fd);
+	process_exit(0);
+
+	return 0;
+}
