59d8bada9f693726ac6010c3c0f60df19fe3d129
Jack Mazanec <jmazane1@nd.edu>
2018-12-01 15:40:04 -0500

    reverted pipe read and pipe read nonblock changes

--- kernel/pipe.h
+++ kernel/pipe.h
@@ -27,6 +27,7 @@ void pipe_flush(struct pipe *p);
 int pipe_set_blocking(struct pipe *p, int b);
 
 int pipe_write(struct pipe *p, char *buffer, int size);
-int pipe_read_internal(struct pipe *p, char *buffer, int size, int block);
+int pipe_read(struct pipe *p, char *buffer, int size);
+int pipe_read_nonblock(struct pipe *p, char *buffer, int size);
 
 #endif
