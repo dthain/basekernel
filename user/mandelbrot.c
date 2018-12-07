9b33467184a030e3bdae89ae57e8c384e4035654
Jack Mazanec <jmazane1@nd.edu>
2018-12-07 17:13:18 -0500

    computes a cooler version of the mandelbrot set

--- user/mandelbrot.c
+++ user/mandelbrot.c
@@ -9,7 +9,7 @@ See the file LICENSE for details.
 #include "library/string.h"
 #include "library/user-io.h"
 
-#define ITER_THRES 40000000
+#define ITER_THRES 100
 #define VAL_THRES 2
 #define STEPS 5
 
@@ -18,19 +18,20 @@ See the file LICENSE for details.
 */
 
 typedef struct Complex {
-	int r;
-	int i;
+	float r;
+	float i;
 } Complex;
 
 int in_set(Complex c);
-void draw_border(int x, int y, int w, int h, int thickness, int r, int g, int b);
-void plot_point(Complex c, int iter_val, int origin);
+void plot_point(int iter_val, int j, int k);
 
 int main(const char ** argv, int argc) 
 {
 	/* Set up params */
 	int dim = 400;
-	int orgin = dim/2;
+	float r_init = 0.3;
+	float i_init = -0.19;
+	float factor = 0.001;
 
 	/* Setup the window */
 	int wd = open_window(KNO_STDWIN, 0, 0, dim, dim);
@@ -39,46 +40,24 @@ int main(const char ** argv, int argc)
 
 	/* For each point, see if it is in the Mandelbrot set */
 	Complex current;
-	int iter_val, i, j, k;
-	for (i = 0; i < 4; ++i)
-	{
-		for (j = 0; j < orgin; ++j)
-		{
-			for (k = 0; k < orgin; ++k)
-			{
-				process_sleep(100);
-				if (i == 0) {
-					current.r = j;
-					current.i = k;
-				} else if (i == 1) {
-					current.r = -j;
-					current.i = k;
-				} else if (i == 2) {
-					current.r = -j;
-					current.i = -k;
-				} else {
-					current.r = j;
-					current.i = -k;
-				}
-				iter_val = in_set(current);
-				plot_point(current, iter_val, orgin);
-				draw_window(wd);
-				draw_flush();
-			}
-		}
+	int iter_val, j, k;
 
+	for (j = 0; j < dim; ++j)
+	{
+		for (k = 0; k < dim; ++k)
+		{
+			current.r = (float)j*factor + r_init;
+			current.i = (float)k*factor + i_init;
+			iter_val = in_set(current);
+			plot_point(iter_val, j, k);
+			draw_window(wd);
+			draw_flush();
+		}
+		process_yield();
 	}
 
-	return 0;
-}
 
-void draw_border(int x, int y, int w, int h, int thickness, int r, int g, int b) 
-{
-	draw_color(r, b, g);
-	draw_rect(x, y, w, thickness);
-	draw_rect(x, y, thickness, h);
-	draw_rect(x + w - thickness, y, thickness, h);
-	draw_rect(x, y + h - thickness, w, thickness);
+	return 0;
 }
 
 int in_set(Complex c)
@@ -91,27 +70,27 @@ int in_set(Complex c)
 		z.i = 2*z.r*z.i + c.i;
 
 		if (z.r > VAL_THRES) {
-			return ITER_THRES + 1;
+			return i;
 		}
 	}
 
-	return i;
+	return i + 1;
 }
 
-void plot_point(Complex c, int iter_val, int orgin)
+void plot_point(int iter_val, int j, int k)
 {
 	int step_size = ITER_THRES/STEPS;
 	int colors[STEPS][3] = {{255,255,0},{255,215,0},{50,205,50},{0,0,205},{165,42,42}};
 
 	if (iter_val > ITER_THRES) {
 		draw_color(255, 255, 255);
-		draw_rect(c.r + orgin, c.i + orgin, 1, 1);
+		draw_rect(j, k, 1, 1);
 	} else {
 		for (int i = 0; i < STEPS; ++i)
 		{
 			if (iter_val <= step_size*(i+1)) {
 				draw_color(colors[i][0], colors[i][1], colors[i][2]);
-				draw_rect(c.r + orgin, c.i + orgin, 1, 1);
+				draw_rect(j, k, 1, 1);
 				break;
 			}
 		}
