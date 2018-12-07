d35efce71babee966a0fd4c4eb59b01be0a88630
Jack Mazanec <jmazane1@nd.edu>
2018-12-07 16:48:17 -0500

    use new sys call to get time

--- user/clock.c
+++ user/clock.c
@@ -8,73 +8,59 @@ See the file LICENSE for details.
 #include "library/syscalls.h"
 #include "library/string.h"
 #include "library/user-io.h"
+#include "kernel/types.h"
 
 /* 
 	Display time in a window
 */
 
-
 /* Function declarations */
 void draw_border(int x, int y, int w, int h, int thickness, int r, int g, int b);
-void draw_clock(uint32_t hour, uint32_t mins, int x, int y, int padding, int r, int g, int b, int tick);
+void draw_clock(uint32_t hour, uint32_t minute, int timezone, int military, int x, int y, int padding, int r, int g, int b);
 
 int main(const char ** argv, int argc) 
 {
-	/* User needs to input initial time I guess */
-	printf("%d\n", argc);
-	uint32_t hour = (argv[1][0] - '0')*10 + (argv[1][1] - '0');
-	uint32_t mins = (argv[1][3] - '0')*10 + (argv[1][4] - '0');
+	/* Configure clock params */
+	int military = 0;
+	if (argc > 1 && strcmp(argv[1], "military") == 0)
+	{
+		military = 1;
+	}
+	int timezone = 5;
 
-	/* Clock params */
+	/* Get initial time */
+	struct rtc_time time;
+	gettimeofday_rtc(&time);
+
+	/* Clock draw params */
 	int CLOCK_W = 55;
 	int CLOCK_H = 25;
 	int thickness = 4;
-	
+
 	/* Set up window  */
-	//int wd = open_window(KNO_STDWIN, 0, 0, CLOCK_W, CLOCK_H);
 	draw_window(KNO_STDWIN);
 	draw_clear(0, 0, CLOCK_W, CLOCK_H);
 	draw_border(0, 0, CLOCK_W, CLOCK_H, thickness, 255, 255, 255);
-	draw_clock(hour, mins, 0, 0, 2*thickness, 255, 255, 255, 1);
+	draw_clock(time.hour, time.minute, timezone, military, 0, 0, 2*thickness, 255, 255, 255);
 	draw_flush();
 
 	/* Run Clock */
 	char c = 0;
-	int tick = 0;
-	int seconds = 0;
 	while(c != 'q')
 	{
 		read_nonblock(0, &c, 1);
 		process_sleep(2000);
-		seconds++;
-		if (seconds == 60)
-		{
-			seconds = 0;
-			mins++;
-		}
-
-		if (mins == 60)
-		{
-			mins = 0;
-			hour++;
-		}
-
-		if (hour == 13)
-		{
-			hour = 1;
-		}
-
-		draw_clock(hour, mins, 0, 0, 2*thickness, 255, 255, 255, tick);
-		tick = (tick == 0) ? 1:0;
+		gettimeofday_rtc(&time);
+		draw_clock(time.hour, time.minute, timezone, military, 0, 0, 2*thickness, 255, 255, 255);
 		draw_flush();
 
 	}
 
-
 	return 0;
 }
 
-void draw_border(int x, int y, int w, int h, int thickness, int r, int g, int b) {
+void draw_border(int x, int y, int w, int h, int thickness, int r, int g, int b) 
+{
 	draw_color(r, b, g);
 	draw_rect(x, y, w, thickness);
 	draw_rect(x, y, thickness, h);
@@ -82,30 +68,35 @@ void draw_border(int x, int y, int w, int h, int thickness, int r, int g, int b)
 	draw_rect(x, y + h - thickness, w, thickness);
 }
 
-void draw_clock(uint32_t hour, uint32_t mins, int x, int y, int padding, int r, int g, int b, int tick) {
+void draw_clock(uint32_t hour, uint32_t minute, int timezone, int military, int x, int y, int padding, int r, int g, int b) {
 	draw_color(r, b, g);
 
 	char h_str[100];
 	char m_str[100];
 
+	int tz_hour = (int)hour - timezone;
+	if (tz_hour < 0) 
+	{
+		tz_hour += 24;
+	}
 
-	uint_to_string(hour, h_str);
-	uint_to_string(mins, m_str);
+	if (military == 0 && tz_hour > 12) 
+	{
+		tz_hour -= 12;
+	}
+
+	uint_to_string((uint32_t) tz_hour, h_str);
+	uint_to_string(minute, m_str);
 
 	char time[100] = "";
 
-	if (strlen(h_str) == 1)
+	if (strlen(h_str) == 1) 
 	{
-		strcat(time, "0");
+		strcat(time, " ");
 	}
 
 	strcat(time, (const char *) h_str);
-	if (tick) {
-		strcat(time, ":");
-	} else {
-		strcat(time, " ");
-	}
-	
+	strcat(time, ":");
 	strcat(time, (const char *) m_str);
 
 	draw_string(x + padding, y + padding, time);
