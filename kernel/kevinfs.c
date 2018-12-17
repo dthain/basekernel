2634d6de85644a1743569ae4990505b5718e15d2
Ethan Williams <ewilli23@nd.edu>
2018-12-16 21:09:19 -0600

    Removed ALL block-sized stack allocations from kevinfs

--- kernel/kevinfs.c
+++ kernel/kevinfs.c
@@ -105,14 +105,16 @@ struct kevinfs_superblock *kevinfs_ata_read_superblock(struct device *device)
 
 static int kevinfs_ata_write_superblock(struct device *device)
 {
-	uint8_t wbuffer[FS_BLOCKSIZE];
+	uint8_t * wbuffer = kmalloc(FS_BLOCKSIZE);
 	uint32_t num_blocks;
 	int ata_blocksize;
 	uint32_t superblock_num_blocks,  available_blocks, free_blocks,
 		 total_inodes, total_inode_bitmap_bytes, total_block_bitmap_bytes, inode_sector_size,
 		 inode_bit_sector_size, data_bit_sector_size;
-	if (!ata_probe(device->unit, &num_blocks, &ata_blocksize, 0) || num_blocks == 0)
+	if (!ata_probe(device->unit, &num_blocks, &ata_blocksize, 0) || num_blocks == 0) {
+		kfree(wbuffer);
 		return -1;
+	}
 
 	char zeros[ata_blocksize];
 	memset(zeros, 0, ata_blocksize);
@@ -143,16 +145,20 @@ static int kevinfs_ata_write_superblock(struct device *device)
 	uint32_t counter = 0;
 	printf("Writing inode bitmap...\n");
 	for (uint32_t i = super.inode_bitmap_start; i < super.inode_start; i++) {
-		if(!bcache_write(device, zeros, 1, i))
+		if(!bcache_write(device, zeros, 1, i)) {
+			kfree(wbuffer);
 			return -1;
+		}
 		counter++;
 	}
 	printf("%u blocks written\n", counter);
 	counter = 0;
 	printf("Writing free block bitmap...\n");
 	for (uint32_t i = super.block_bitmap_start; i < super.free_block_start; i++) {
-		if(!bcache_write(device, zeros, 1, i))
+		if(!bcache_write(device, zeros, 1, i)) {
+			kfree(wbuffer);
 			return -1;
+		}
 		counter++;
 	}
 	printf("writing superblock...\n");
@@ -161,6 +167,7 @@ static int kevinfs_ata_write_superblock(struct device *device)
 	printf("flushing dirty blocks...\n");
 	bcache_flush_device(device);
 	printf("%u blocks written\n", counter);
+	kfree(wbuffer);
 	return counter;
 }
 
@@ -173,9 +180,11 @@ int kevinfs_ata_format(struct device *device)
 static int kevinfs_ata_get_available_bit(struct device *device, uint32_t index, uint32_t * res)
 {
 	uint32_t bit_index;
-	uint8_t bit_buffer[FS_BLOCKSIZE];
-	if(kevinfs_ata_read_block(device, index, bit_buffer) < 0)
+	uint8_t * bit_buffer = kmalloc(FS_BLOCKSIZE);
+	if(kevinfs_ata_read_block(device, index, bit_buffer) < 0) {
+		kfree(bit_buffer);
 		return -1;
+	}
 
 	for(bit_index = 0; bit_index < sizeof(bit_buffer); bit_index++) {
 		if(bit_buffer[bit_index] != 255) {
@@ -184,49 +193,63 @@ static int kevinfs_ata_get_available_bit(struct device *device, uint32_t index, 
 			for(offset = 0; offset < sizeof(uint8_t) * 8; offset += 1) {
 				if(!(bit_buffer[bit_index] & bit)) {
 					*res = index * FS_BLOCKSIZE + bit_index * sizeof(uint8_t) * 8 + offset;
+					kfree(bit_buffer);
 					return 0;
 				}
 				bit >>= 1;
 			}
 		}
 	}
+	kfree(bit_buffer);
 	return -1;
 }
 
 int kevinfs_ata_set_bit(struct device *device, uint32_t index, uint32_t begin, uint32_t end)
 {
-	uint8_t bit_buffer[FS_BLOCKSIZE];
+	uint8_t * bit_buffer = kmalloc(FS_BLOCKSIZE);
 	uint32_t bit_block_index = index / (8 * FS_BLOCKSIZE);
 	uint32_t bit_block_offset = index % (8 * FS_BLOCKSIZE);
 	uint8_t bit_mask = 1u << (7 - bit_block_offset % 8);
 
-	if(kevinfs_ata_read_block(device, begin + bit_block_index, bit_buffer) < 0)
+	if(kevinfs_ata_read_block(device, begin + bit_block_index, bit_buffer) < 0) {
+		kfree(bit_buffer);
 		return -1;
-	if((bit_mask & bit_buffer[bit_block_offset / 8]) > 0)
+	}
+	if((bit_mask & bit_buffer[bit_block_offset / 8]) > 0) {
+		kfree(bit_buffer);
 		return -1;
+	}
 
 	bit_buffer[bit_block_offset / 8] |= bit_mask;
-	return kevinfs_ata_write_block(device, begin + bit_block_index, bit_buffer);
+	int res = kevinfs_ata_write_block(device, begin + bit_block_index, bit_buffer);
+	kfree(bit_buffer);
+	return res;
 }
 
 int kevinfs_ata_unset_bit(struct device *device, uint32_t index, uint32_t begin, uint32_t end)
 {
-	uint8_t bit_buffer[FS_BLOCKSIZE];
+	uint8_t * bit_buffer = kmalloc(FS_BLOCKSIZE);
 	uint32_t bit_block_index = index / (8 * FS_BLOCKSIZE);
 	uint32_t bit_block_offset = index % (8 * FS_BLOCKSIZE);
 	uint8_t bit_mask = 1u << (7 - bit_block_offset % 8);
 
-	if(kevinfs_ata_read_block(device, begin + bit_block_index, bit_buffer) < 0)
+	if(kevinfs_ata_read_block(device, begin + bit_block_index, bit_buffer) < 0) {
+		kfree(bit_buffer);
 		return -1;
-	if((bit_mask & bit_buffer[bit_block_offset / 8]) == 0)
+	}
+	if((bit_mask & bit_buffer[bit_block_offset / 8]) == 0) {
+		kfree(bit_buffer);
 		return -1;
+	}
 	bit_buffer[bit_block_offset / 8] ^= bit_mask;
-	return kevinfs_ata_write_block(device, begin + bit_block_index, bit_buffer);
+	int res = kevinfs_ata_write_block(device, begin + bit_block_index, bit_buffer);
+	kfree(bit_buffer);
+	return res;
 }
 
 int kevinfs_ata_check_bit(struct device *device, uint32_t index, uint32_t begin, uint32_t end, bool * res)
 {
-	uint8_t bit_buffer[FS_BLOCKSIZE];
+	uint8_t * bit_buffer = kmalloc(FS_BLOCKSIZE);
 	uint32_t bit_block_index = index / (8 * FS_BLOCKSIZE);
 	uint32_t bit_block_offset = index % (8 * FS_BLOCKSIZE);
 	uint8_t bit_mask = 1u << (7 - bit_block_offset % 8);
@@ -235,6 +258,7 @@ int kevinfs_ata_check_bit(struct device *device, uint32_t index, uint32_t begin,
 		return -1;
 	}
 	*res = (bit_mask & bit_buffer[bit_block_offset / 8]) != 0;
+	kfree(bit_buffer);
 	return 0;
 }
 
@@ -324,13 +348,14 @@ static struct kevinfs_inode *kevinfs_lookup_inode(struct kevinfs_volume *kv, uin
 	uint32_t block = index / inodes_per_block;
 	uint32_t offset = index % inodes_per_block;
 	bool is_active;
-	struct kevinfs_inode current_nodes[inodes_per_block + 1];
+	// struct kevinfs_inode current_nodes[inodes_per_block + 1]; // THIS IS BAD
 
 	if(kevinfs_ata_check_bit(kv->device, index, super->inode_bitmap_start, super->inode_start, &is_active) < 0)
 		return 0;
 	if(is_active == 0)
 		return 0;
 
+	struct kevinfs_inode *current_nodes = kmalloc((inodes_per_block + 1)*sizeof(struct kevinfs_inode));
 	node = kmalloc(sizeof(struct kevinfs_inode));
 	if(node) {
 		if(kevinfs_ata_read_block(kv->device, super->inode_start + block, current_nodes) < 0) {
@@ -341,6 +366,7 @@ static struct kevinfs_inode *kevinfs_lookup_inode(struct kevinfs_volume *kv, uin
 		}
 	}
 
+	kfree(current_nodes);
 	return node;
 }
 
