247b399c099f3f7ee2816abf0814b531407d57b2
Douglas Thain <dthain@nd.edu>
2019-01-11 15:44:53 -0500

    Move validation test into conditional kernel code.

--- kernel/is_valid.c
+++ kernel/is_valid.c
@@ -104,3 +104,58 @@ int is_valid_string( const char *str )
 	return 1;
 }
 
+#ifdef TEST
+void is_valid_test()
+{
+	printf("Testing PATH validations...\n");
+	char * s = "/root/is-valid.path";
+	if (!is_valid_path(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = "abcdefghijklmnopqrstuvwxyz";
+	if (!is_valid_path(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	if (!is_valid_path(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = "0123456789";
+	if (!is_valid_path(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = "/root/isn't/valid:";
+	if (is_valid_path(s))
+		printf("ERROR: Did not invalidate %s\n", s);
+
+	printf("Testing Tag Validations...\n");
+	s = "TAG";
+	if (!is_valid_tag(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = "abcdefghijklmnopqrstuvwxyz";
+	if (!is_valid_tag(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	if (!is_valid_tag(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = "0123456789";
+	if (!is_valid_tag(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = ":NOT-A-TAG";
+	if (is_valid_tag(s))
+		printf("ERROR: Did not invalidate %s\n", s);
+
+	printf("Testing Location Validations...\n");
+	s = "HOME:path/to/root/dir";
+	if (!is_valid_location(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = "TAG";
+	if (!is_valid_tag(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = "/root/is-valid.path";
+	if (!is_valid_path(s))
+		printf("ERROR: Did not validate %s\n", s);
+	s = "BAD-INTENT:p@th/:/root/dir";
+	if (is_valid_location(s))
+		printf("ERROR: Did not invalidate %s\n", s);
+
+}
+
+#endif
+
