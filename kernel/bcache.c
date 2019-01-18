4e8d2afbc8e5789e47249cd0ae265208ff4d3e1f
Douglas Thain <dthain@nd.edu>
2019-01-18 10:06:42 -0500

    Update copyright boilerplate for consistency.

--- kernel/bcache.c
+++ kernel/bcache.c
@@ -1,3 +1,8 @@
+/*
+Copyright (C) 2015-2019 The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
 
 #include "bcache.h"
 #include "list.h"
--- kernel/hash_set.h
+++ kernel/hash_set.h
@@ -1,3 +1,9 @@
+/*
+Copyright (C) 2016-2019 The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
+
 #ifndef HASH_SET_H
 #define HASH_SET_H
 
