427fad45e647579fb3f2029266670cf355459627
Jack Mazanec <jmazane1@nd.edu>
2019-01-21 10:00:05 -0500

    fixed order of argc, argv in exec

--- user/exectest.c
+++ user/exectest.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	if (pid == 0) {
 		printf("hello world, I am the child %d.\n", syscall_process_self());
 		const char *args[] = { "snake.exe" };
-		syscall_process_exec("snake.exe", args, 1);
+		syscall_process_exec("snake.exe", 1, args);
 	} else {
 		printf("hello world, I am the parent %d.\n", syscall_process_self());
 		struct process_info info;
