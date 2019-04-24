fb7dee8a64954372c8056181d6c412582fb2572c
Jack Mazanec <jmazane1@nd.edu>
2019-04-24 09:39:43 -0400

    removed structs related to object syscalls

--- include/kernel/stats.h
+++ include/kernel/stats.h
@@ -4,23 +4,10 @@
 #include "kernel/types.h"
 #include "kernel/syscall.h"
 
-#define OBJECT_STATS_LEVEL 1
-#define CONSOLE_STATS_LEVEL 2
-#define GRAPHICS_STATS_LEVEL 2
-#define DEVICE_STATS_LEVEL 2
-#define PIPE_STATS_LEVEL 2
-#define FILE_STATS_LEVEL 2
-#define DIR_STATS_LEVEL 2
-
 struct system_stats {
 	int time;
 	int blocks_read[4];
 	int blocks_written[4];
-};
-
-struct device_stats {
-	int reads;
-	int writes;
 };
 
 struct device_driver_stats {
@@ -34,32 +21,6 @@ struct bcache_stats {
 	int write_hits;
 	int write_misses;
 	int writebacks;
-};
-
-struct console_stats {
-	int writes;
-	uint64_t bytes_written;
-};
-
-struct file_stats {
-	int reads;
-	int writes;
-};
-
-struct graphics_stats {
-	int writes;
-};
-
-struct object_stats {
-	int reads;
-	int writes;
-	uint64_t bytes_read;
-	uint64_t bytes_written;
-};
-
-struct pipe_stats {
-	int reads;
-	int writes;
 };
 
 struct process_stats {
