308a6e1192d39fbfdbc725ab66203e6f9f8a3d0c
Jack Mazanec <jmazane1@nd.edu>
2019-04-24 09:33:20 -0400

    added more stats structs

--- include/kernel/stats.h
+++ include/kernel/stats.h
@@ -4,33 +4,70 @@
 #include "kernel/types.h"
 #include "kernel/syscall.h"
 
+#define OBJECT_STATS_LEVEL 1
+#define CONSOLE_STATS_LEVEL 2
+#define GRAPHICS_STATS_LEVEL 2
+#define DEVICE_STATS_LEVEL 2
+#define PIPE_STATS_LEVEL 2
+#define FILE_STATS_LEVEL 2
+#define DIR_STATS_LEVEL 2
+
 struct system_stats {
-	uint32_t time;
-	uint32_t blocks_read[4];
-	uint32_t blocks_written[4];
+	int time;
+	int blocks_read[4];
+	int blocks_written[4];
+};
+
+struct device_stats {
+	int reads;
+	int writes;
+};
+
+struct device_driver_stats {
+	int blocks_written;
+	int blocks_read;
 };
 
 struct bcache_stats {
-	uint32_t read_hits;
-	uint32_t read_misses;
-	uint32_t write_hits;
-	uint32_t write_misses;
-	uint32_t writebacks;
+	int read_hits;
+	int read_misses;
+	int write_hits;
+	int write_misses;
+	int writebacks;
+};
+
+struct console_stats {
+	int writes;
+	uint64_t bytes_written;
+};
+
+struct file_stats {
+	int reads;
+	int writes;
+};
+
+struct graphics_stats {
+	int writes;
 };
 
 struct object_stats {
-	uint32_t reads;
-	uint32_t writes;
+	int reads;
+	int writes;
 	uint64_t bytes_read;
 	uint64_t bytes_written;
 };
 
+struct pipe_stats {
+	int reads;
+	int writes;
+};
+
 struct process_stats {
-	uint32_t blocks_read;
-	uint32_t blocks_written;
-	uint32_t bytes_read;
-	uint32_t bytes_written;
-	uint32_t syscall_count[MAX_SYSCALL];
+	int blocks_read;
+	int blocks_written;
+	int bytes_read;
+	int bytes_written;
+	int syscall_count[MAX_SYSCALL];
 };
 
 #endif
