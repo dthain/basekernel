3f40760f68525d93bdae2fd12f6da1a0883e66d5
Jack Mazanec <jmazane1@nd.edu>
2019-05-01 10:02:02 -0400

    Revert "move bcache stats struct to stats.h"
    This reverts commit fca3ebd0d3b48d9f280c5195fb001546fa91358a.

--- kernel/bcache.h
+++ kernel/bcache.h
@@ -8,7 +8,6 @@ See the file LICENSE for details.
 #define BCACHE_H
 
 #include "device.h"
-#include "kernel/stats.h"
 
 int  bcache_read( struct device *d, char *data, int blocks, int offset );
 int  bcache_write( struct device *d, const char *data, int blocks, int offset );
@@ -19,6 +18,14 @@ int  bcache_write_block( struct device *d, const char *data, int block );
 void bcache_flush_block( struct device *d, int block );
 void bcache_flush_device( struct device *d  );
 void bcache_flush_all();
+
+struct bcache_stats {
+	unsigned read_hits;
+	unsigned read_misses;
+	unsigned write_hits;
+	unsigned write_misses;
+	unsigned writebacks;
+};
 
 void bcache_get_stats( struct bcache_stats *s );
 
