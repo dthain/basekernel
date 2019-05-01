08f156dbb71c5e4211ca27e4103674dadc244d85
Jack Mazanec <jmazane1@nd.edu>
2019-05-01 10:06:18 -0400

    Revert "added syscall for bcache stats"
    This reverts commit 28e969d7cb553fa6110a09bdc1a1dde4982d276d.

--- include/library/syscalls.h
+++ include/library/syscalls.h
@@ -60,10 +60,6 @@ int syscall_object_max();
 /* Syscalls that query or affect the whole system state. */
 
 int syscall_system_stats(struct system_stats *s);
-int syscall_bcache_stats(struct bcache_stats *s);
-
-int syscall_bcache_flush();
-
 int syscall_system_time( uint32_t *t );
 int syscall_system_rtc( struct rtc_time *t );
 
