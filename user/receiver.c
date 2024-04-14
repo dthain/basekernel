6c4db0ea37783526d96cae29e908a3a5e6d6f092
mlabib2 <mahirlive2000@gmail.com>
2024-04-14 12:23:53 +0800

    remove sender.c and receiver.c

--- user/receiver.c
+++
@@ -1,8 +0,0 @@
-// receiver program
-
-// can uncomment the bottom part
-
-
-// char * fname = "/path/to/file/filename"; int res = syscall_make_named_pipe(fname);
-// int fd = syscall_open_named_pipe(fname);
-// char buffer[20]; syscall_object_read(fd, buffer, 20); printf("%s\n", buffer); 
--- user/sender.c
+++
@@ -1,6 +0,0 @@
-//sender program
-
-// can uncomment the bottom part
-
-// char *fname = "/path/to/file/filename"; int fd = syscall_open_named_pipe(fname);
-// char buffer[] = "Hello World\n"; syscall_object_write(fd,buffer,strlen(buffer));
